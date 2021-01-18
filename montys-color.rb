0b5091fc1f517bc66c1a76033abfb96eee1fdd0d
xi-er <t.vasundhra2@gmail.com>
2021-01-18 12:06:09 +0530

    else

--- montys-color.rb
+++ montys-color.rb
@@ -4,11 +4,7 @@ puts "What is your favorite color? enter color"
 color = gets.chop
 #puts "Wow! My favorite color is #{color} also!"
 
-# Lesson 4: Using an If Statement
 
-#if color == "blue"
-   # puts "Wow! My favorite color is #{color} also!"
- #end
 
 # Lesson 5: Adding an Else Statement
 
